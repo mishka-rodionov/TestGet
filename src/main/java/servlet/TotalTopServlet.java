35dcb315e9998ba34707a5342e354e4d74cebd9a
Mikhail Rodionov <rodionov.mikhail.a@gmail.com>
2018-06-28 11:46:43 +0400

    temp

--- src/main/java/servlet/TotalTopServlet.java
+++ src/main/java/servlet/TotalTopServlet.java
@@ -25,7 +25,7 @@ public class TotalTopServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
         username = request.getParameter(Data.getUsername());
         org.json.JSONObject topTotal = DBManager.getTopTotal();
